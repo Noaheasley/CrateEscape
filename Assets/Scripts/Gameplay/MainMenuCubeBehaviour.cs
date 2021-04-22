66cab514da696d02681d9b708fd6df51065683ed
Noaheasley <69812929+Noaheasley@users.noreply.github.com>
2021-04-22 17:37:28 -0500

    added main menu

---
+++ Assets/Scripts/Gameplay/MainMenuCubeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCubeBehaviour : MonoBehaviour
+{
+    public float x = 1.0f;
+    public float y = 1.0f;
+    public float z = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(x,y,z);
+    }
+}
---
+++ Assets/Scripts/Gameplay/StartGameScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour
+{
+    public string newGameScene;
+    //loads up the game
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+}
